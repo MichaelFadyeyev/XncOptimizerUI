030809bca525074ea9ff840c5c2655b05ffc7aaa
Михайло Фадєєв <mihail.fadeev@outlook.com>
2026-09-07 14:49:14 +0300

    Add GroovingToolWidths and MillingToolDiams to AppOptions

--- XncOptimizerUI/Configuration/AppOptions.cs
+++ XncOptimizerUI/Configuration/AppOptions.cs
@@ -5,5 +5,10 @@
         public decimal SawWidth { get; set; } = 4.0m;
         public List<string> LabelsToProcess { get; set; } = ["поріз.2х40"];
         public int LastLabelToProcessSelectedIndex { get; set; } = 0;
+
+        public List<decimal> GroovingToolWidths { get; set; } = [2.8m, 3.2m];
+
+        public List<decimal> MillingToolDiams { get; set; } = [6.0m, 10.0m, 20.0m];
     }
+    
 }
